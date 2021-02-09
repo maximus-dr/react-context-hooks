8c595816a4d64918f4ff524f0ac70b2afbcd6bbf
Max Ivanov <maximus-dr@mail.ru>
2021-02-10 00:44:06 +0300

    Basic styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+const { default: BookList } = require("./components/BookList");
+const { default: Navbar } = require("./components/Navbar");
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar />
+      <BookList />
     </div>
   );
 }
---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function BookList() {
+  return (
+    <div className="book-list">
+      <ul>
+        <li>the way of kings</li>
+        <li>the name of wind</li>
+        <li>the final empire</li>
+      </ul>
+    </div>
+  )
+}
